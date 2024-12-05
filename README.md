8cafd9d6555b7c9c209e79a2f9a84a3d8d5aef44
bowman-crego <bowmanccrego@gmail.com>
2024-12-05 13:58:53 -0800

    Added a README using node.js

---
+++ README.md
@@ -0,0 +1,19 @@
+# Generate a README File using Node.JS
+
+## Description
+I have successfully created a README file using node.js 
+
+## Installation Instructions
+In order to get this working I used a generateMArkdown method to return data and render that data to the README file. Before that step I wa able to scuessfully creat an object containing the varibales within  the inquirer package. Inquirer must be installed for this to work. 
+
+## Usage Information
+This is the section for usage information.
+
+## Table of Contents
+https://github.com/bowman-crego
+
+bowmanccrego@gmail.com
+
+## License
+mit badge goes here;
+mit link goes here
--- index.js
+++ index.js
@@ -1,13 +1,77 @@
 // TODO: Include packages needed for this application
+import inquirer from "inquirer";
+import fs from "fs";
+import generateMarkdown from "./utils/generateMarkdown.js";
+import Choice from "inquirer/lib/objects/choice.js";
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        message: "What is your Project Title?",
+        name: "title",
+        type:"input"
+
+    },
+    {
+        message: "What is your Description?",
+        name: "description",
+        type:"input"
+
+    }, 
+    {
+        message: "What is your Installation Instructions?",
+        name: "instructions",
+        type:"input"
+    }, 
+    {
+        message:"Describe the Usage Information",
+        name: "usage",
+        type: "input"
+    },
+    {
+        message: "Please choose a license?",
+        name: "license",
+        type:"list",
+        choices: ["MIT", "BSD3", "none"]
+    },
+    {
+        message: "What is your GitHub username?",
+        name: "username",
+        type:"input",
+       
+    },
+    {
+        message: "What is you email address?",
+        name: "email",
+        type:"input",
+    },
+    
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.log('Cannot successfully create readMe', err)
+        } else {
+            console.log(`Successfully wrote to ${'README.md'}`)
+        }
+
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(answers => {
+    
+    let markdown = generateMarkdown(answers);
+    
+    writeToFile("readme.md", markdown)
+    })
+}
 
 // Function call to initialize app
 init();
